5223aa719bb3064896a444bf5544c5f3ef3d64be
miguins <lucasmiguins@gmail.com>
2021-06-02 12:28:19 -0300

    Setup inicial do projeto

---
+++ src/main/java/br/com/zupedu/lucasmiguins/proposta/PropostaApplication.java
@@ -0,0 +1,12 @@
+package br.com.zupedu.lucasmiguins.proposta;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PropostaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PropostaApplication.class, args);
+    }
+}
