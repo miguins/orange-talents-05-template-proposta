bd104ceb2534baed10aa2ccaa632c2d07fb703c4
miguins <lucasmiguins@gmail.com>
2021-06-08 17:22:03 -0300

    050 Login via senha

---
+++ src/main/java/br/com/zupedu/lucasmiguins/proposta/config/security/SecurityConfiguration.java
@@ -0,0 +1,29 @@
+package br.com.zupedu.lucasmiguins.proposta.config.security;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.annotation.web.configurers.oauth2.server.resource.OAuth2ResourceServerConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
+
+@EnableWebSecurity
+@Configuration
+public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+
+        http.authorizeRequests()
+                .antMatchers(HttpMethod.POST,"/**/propostas").hasAuthority("SCOPE_propostas:write")
+                .antMatchers(HttpMethod.POST,"/**/biometrias/**").hasAuthority("SCOPE_propostas:write")
+                .antMatchers(HttpMethod.GET,"/**/propostas/**").hasAuthority("SCOPE_propostas:read")
+                .anyRequest().authenticated()
+                .and().cors()
+                .and().csrf().disable()
+                .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+                .and().oauth2ResourceServer(OAuth2ResourceServerConfigurer::jwt);
+        ;
+    }
+}
